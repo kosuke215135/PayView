b3fec80b2108978adab627a8cd1c371e3e59543f
Kouya Arakaki <e215705@ie.u-ryukyu.ac.jp>
2023-12-11 01:12:04 +0900

    __init__.pyの編集

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, session
 import os
 from flask import request
 from db import get_db
 import random
-
+from datetime import timedelta #時間情報を用いるため
+import math
 
 this_dir_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -20,7 +21,75 @@ def create_app():
 
     import search_shop
     app.register_blueprint(search_shop.bp)
+    
 
+    # 現在地からお店まで〜kmか出力する関数
+    def location_distance(user_latitude, user_longitude, store_latitude, store_longitude):
+        # 地球の半径（キロメートル）
+        R = 6371.0
+
+        # 緯度経度をラジアンに変換
+        lat1 = math.radians(user_latitude)
+        lon1 = math.radians(user_longitude)
+        lat2 = math.radians(store_latitude)
+        lon2 = math.radians(store_longitude)
+
+        # 緯度と経度の差
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+
+        # ハーヴァーサイン公式
+        a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+        # 距離
+        distance = R * c
+
+        return distance
+    
+    def get_distanced_lat_lng(lat, lng, distance):
+        R = math.pi / 180  # 円周率をラジアンに変換
+        earth_radius = 6371  # 地球の半径（キロメートル）
+        kilometer_per_degree = R * earth_radius  # 1度あたりのキロメートル
+        degree_per_kilometer = 1 / kilometer_per_degree  # 1キロメートルあたりの度
+        diff = distance * degree_per_kilometer  # 指定された距離を角度に変換
+        return {'n': lat + diff, 'e': lng + diff, 's': lat - diff, 'w': lng - diff}
+
+    # jsからhttp経由のPOST方式で、現在地のデータを受け取る。 by kouya
+    @app.route('/send-location', methods=['POST'])
+    def receive_location():
+        location_data = request.json
+        user_latitude = location_data["latitude"]
+        user_longitude = location_data["longitude"]
+        
+        
+        result = get_distanced_lat_lng(user_latitude, user_longitude, 3)
+        n = str(result["n"])
+        e = str(result["e"])
+        s = str(result["s"])
+        w = str(result["w"])
+                
+        # 3km以内のお店だけをデータベースから指定
+        db = get_db()
+        cur = db.cursor(dictionary=True)
+        query = "select * from shops where ("+n+">latitude and latitude>"+s+") and ("+e+">longitude and longitude>"+w+");"
+        cur.execute(query)
+        shops = cur.fetchall()
+        
+        latitude_longitude_list = []
+        for shop_dict in shops:
+            latitude_longitude = [shop_dict["latitude"], shop_dict["longitude"]]
+            latitude_longitude_list.append(latitude_longitude)
+        
+        distance_list = []
+        # 距離を出す
+        for latitude_longitude in latitude_longitude_list:
+            store_latitude = latitude_longitude[0]
+            store_longitude = latitude_longitude[1]
+            distance = location_distance(user_latitude, user_longitude, store_latitude, store_longitude)
+            distance_list.append(distance)
+        return render_template("top.html", distance_list=distance_list)
+    
     @app.route("/")
     def top():
         db = get_db()
